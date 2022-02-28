18654822e53d67174dc4bdee8d5ac9baccfb65ec
unknown <raifa@list.ru>
2022-02-28 11:00:59 +0300

    create exeption

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,11 +7,11 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click() 
 
-    # def should_be_login_link(self):
-    #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
 
-    # def test_guest_should_see_login_link(browser):
-    #     link = "http://selenium1py.pythonanywhere.com/"
-    #     page = MainPage(browser, link)
-    #     page.open()
-    #     page.should_be_login_link()
+    def test_guest_should_see_login_link(browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
