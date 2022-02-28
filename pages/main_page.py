96e439e3a129ef33771e200f20861fc3bc9d565e
unknown <raifa@list.ru>
2022-02-28 10:11:33 +0300

    create test verify open url

---
+++ pages/main_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+class MainPage(BasePage):
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click() 
+
+    # def should_be_login_link(self):
+    #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
+    # def test_guest_should_see_login_link(browser):
+    #     link = "http://selenium1py.pythonanywhere.com/"
+    #     page = MainPage(browser, link)
+    #     page.open()
+    #     page.should_be_login_link()
