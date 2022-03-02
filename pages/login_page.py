a900a1ff778aecc0cec29b99e350560c76246eb3
unknown <raifa@list.ru>
2022-03-02 08:30:06 +0300

    modified file and new file

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators, MainPageLocators
+
+
+class LoginPage(BasePage):
+
+
+    def should_be_login_url(self):        
+        # проверка что находимся на странице логина
+        assert "login" in self.browser.current_url, "This is not a login page"
+
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "LOGIN FORM is not presented"
+
+    def should_be_register_form(self):
+        # проверкf, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "REGISTER FORM is not presented"
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,17 +1,14 @@
+import imp
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click() 
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def test_guest_should_see_login_link(browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
-        page.open()
-        page.should_be_login_link()
