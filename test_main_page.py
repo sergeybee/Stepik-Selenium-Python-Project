e52c06416f200f47c7a4df68a8645bedcd61098c
sergey <raifa@list.ru>
2022-03-10 12:23:53 +0300

    Modified

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,38 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
-
+# Тестирование наличия логин ссылки пользователя класс MainPage()
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+def test_guest_should_be_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+# Тестирование страницы авторизации и регистрации пользователя класс LoginPage()
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_url()
+
+def test_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_form()
+
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_register_form()
+    
