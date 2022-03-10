7378ecae99ac5002b95ca04f8aa75bc83496eb10
sergey <raifa@list.ru>
2022-03-10 12:24:25 +0300

    Modified

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,9 +1,13 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators, MainPageLocators
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
 
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
 
     def should_be_login_url(self):        
         # проверка что находимся на странице логина
