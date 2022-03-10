1bc2459ee55f232404687e64c9f1d2e41ad77b39
sergey <raifa@list.ru>
2022-03-10 12:07:40 +0300

    Add ProductPageLocators Class

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,18 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
+
     LOGIN_LINK = (By.ID, "registration_link")
 
 class LoginPageLocators():
+
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    TITLE_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
+    MESSAGE_AFTER_ADD_TO_BASKET = (By.CSS_SELECTOR, ".alert-success:first-child .alertinner strong")
+    TOTAL_PRICE_AFTER_ADD_TO_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
