41314def49dff5c07e39159245efbf2f8e5fa035
sergeybee <raifa@list.ru>
2022-03-31 08:54:19 +0300

    Add new class BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,18 +1,24 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
 
-    LOGIN_LINK = (By.ID, "registration_link")
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
-class LoginPageLocators():
 
+class MainPageLocators:
+    LOGIN_LINK2 = (By.ID, "registration_link")
+
+
+class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
 
-class ProductPageLocators():
 
+class ProductPageLocators:
     BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     TITLE_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
     PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
     MESSAGE_AFTER_ADD_TO_BASKET = (By.CSS_SELECTOR, ".alert-success:first-child .alertinner strong")
     TOTAL_PRICE_AFTER_ADD_TO_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
+    # SUCCESS_MESSAGES = (By.CSS_SELECTOR, ".alert-success:first-child")
