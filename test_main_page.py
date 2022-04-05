ed6de977d2832958a2bd0f8f42c39220f6a1c55c
sergeybee <raifa@list.ru>
2022-04-05 12:44:09 +0300

    Заменены ссылки

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,7 @@ from pages.login_page import LoginPage
 class TestLoginFromMainPage(object):
     # Тестирование наличия логин ссылки пользователя класс MainPage()
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
@@ -16,7 +16,7 @@ class TestLoginFromMainPage(object):
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
